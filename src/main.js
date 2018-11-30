abeaa7748f7605b59ddec39998d48d57f5673f6e
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-29 23:21:42 -0800

    HTTP 요청을 위한 VueResource추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import './plugins/vuetify'
 import App from './App.vue'
+
+// HTTP 요청을 위한 라이브러리 로딩
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
